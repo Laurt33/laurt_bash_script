e4875c0fb9bca185616158fcf86cf96ab070c849
Laurt33 <45259321+Laurt33@users.noreply.github.com>
2021-01-29 15:55:29 +0100

    Add files via upload

---
+++ remplacer_nom_fichier.sh
@@ -0,0 +1,8 @@
+  SEARCH_PATH="./"
+  SEARCH="SF88"
+  REPLACE="SF98"
+
+  find ${SEARCH_PATH} -type f -name "*${SEARCH}*" | while read FILENAME ; do
+      NEW_FILENAME="$(echo ${FILENAME} | sed -e "s/${SEARCH}/${REPLACE}/g")";
+      mv "${FILENAME}" "${NEW_FILENAME}";
+  done
